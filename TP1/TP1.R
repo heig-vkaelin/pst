971eb96b24a0cfcfd696bb8ff418afb81f79c7c9
Valentin Kaelin <kalanehd@gmail.com>
2021-10-09 14:47:00 +0200

    feat: some tweaks in text for tp1

--- TP1/TP1.R
+++ TP1/TP1.R
@@ -98,7 +98,8 @@ print(mean(cpus / 3))
 print(median(cpus / 3))
 
 # (3) ajoute un processeur avec comme performance 40:
-# Intuition: médiane changée par la valeur ajoutée et moyenne un peu plus basse
+# Intuition: médiane changée par la valeur ajoutée ou aucun changement
+# et moyenne un peu plus basse
 cpusWith40 <- c(cpus, 40)
 print(mean(cpusWith40))
 print(median(cpusWith40))
@@ -108,7 +109,9 @@ print(sd(cpus))
 print(sd(cpus2))
 
 # L'écart-type n'est pas un indicateur très robuste. Il est fortement impacté
-# par les valeurs extrêmes, comme l'exemple ci-dessus le montre.
+# par les valeurs extrêmes, comme l'exemple ci-dessus le montre. Car il est
+# calculé en mettant au carré la différence avec la moyenne, ce qui accentue
+# encore plus l'impact des valeurs extrêmes.
 
 # ----------------------------------------------------------------
 # Exercice 4. Différences à l'examen entre groupes
@@ -120,15 +123,10 @@ notesB = examen[examen$groupe == "B", 2]
 
 graphics::boxplot(
   notesA,
+  notesB, 
   ylab="note",
-  main="Notes du groupe A",
-  ylim = c(1, 6)
-)
-graphics::boxplot(
-  notesB,
-  ylab="note",
-  main="Notes du groupe B",
-  ylim = c(1, 6)
+  names = c("Groupe A", "Groupe B"),
+  main = "Notes séparées entre les 2 groupes"
 )
 
 summary(notesA)
@@ -139,26 +137,31 @@ summary(notesB)
 # vers 4.5. La meilleure note de chaque groupe est également très espacée.
 # Celle du groupe A se trouve vers 4.5 et celle du groupe B vers 6.
 
-# Pour la dispersion, elles sont assez semblabls car on observe que les tailles
+# Pour la dispersion, elles sont assez semblables car on observe que les tailles
 # des deux boxplots entre le 1er et le 4ème quartiles sont plus ou moins
 # similaires.
 # Cependant, le 50% du corps de la boîte du groupe A est plus grand que celui
-# du groupe B. Ce qui signifie que les notes du groupe A sont plus rapprochées
-# au alentours de la médiane.
+# du groupe B. Ce qui signifie que les notes du groupe B sont plus rapprochées
+# au alentours de la médiane et que celles du groupes A sont légèrement plus
+# dispersées. On observe également que les notes du groupe A sont plus étendues
+# sous la médiane et que c'est l'inverse pour le groupe B.
+
+# On peut donc affirmer que le groupe B a mieux réussi l'examen que le groupe A
+# mais qu'il s'agit surtout des extremums qui font pencher le plus la balance.
 
 # c)
 cat("Ecart-type notes du groupe A:", sd(notesA))
 cat("Ecart-type notes du groupe B:", sd(notesB))
 
 # Les deux écarts-types sont assez similaires entre les deux groupes.
-# Cela implique que la différence de dispersion entre les deux groupes n'est
+# Cela explique que la différence de dispersion entre les deux groupes n'est
 # pas énorme. La dispersion des notes du groupe A est donc très légèrement plus
 # grande que celle du groupe B.
 
 # d)
-# Le groupe B a mieux réussi l'examen que le groupe A. De plus, les notes des 
-# deux groupes sont plus ou moins aussi dispersées les unes que les autres.
-# On peut finir par dire que les extremums de chaque groupe sont vraiment les 
-# valeurs qui changent le plus.
-# En effet, la note minimale du groupe A est beaucoup plus basse que celle du
-# groupe B et la note maximale de B est bien plus haute que celle de A.
+# Les conclusions sont les mêmes pour les points b) et c): la dispersion est
+# assez semblable pour les deux groupes. Cependant, dans les deux cas il s'agit
+# du groupe A qui semble être légèrement plus dispersé (au niveau des 
+# écarts-types et de l'espacement interquartile). On peut donc affirmer que le
+# groupe A a légèrement plus de dispersion si ces petites différences nous
+# intéressent.
