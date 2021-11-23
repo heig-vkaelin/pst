6e904851871b8cee2feae65916c245739d9ffcc5
Valentin Kaelin <kalanehd@gmail.com>
2021-11-23 14:45:11 +0100

    feat: add ex 6.1 + 6.2

---
+++ Exercices/ExercicesChap6.R
@@ -0,0 +1,36 @@
+setwd("/Users/Valentin/code/heig/pst/Exercices")
+
+# Exemples du prof
+plot(0:10, dpois(0:10, 6)) # proba de qqch qui arrive 6x par minute
+
+# ----------------------------------------------------------------
+# Exercice 1
+# ----------------------------------------------------------------
+
+# a)
+plot(0:20, dbinom(0:20, 20, 0.5)) # 21 coefficiants de la proba 0.5
+plot(0:20, dbinom(0:20, 20, 1/3)) # 21 coefficiants de la proba 1/3
+
+
+# ----------------------------------------------------------------
+# Exercice 2
+# ----------------------------------------------------------------
+
+# a)
+x = 2 # nb de succès voulu
+n = 20  # nb de tests
+proba = 0.1 # la proba que la communication échoue
+pbinom(x, n, proba, lower.tail = FALSE)
+
+1 - sum(dbinom(0:2, 20, .1)) # variante du prof
+
+# b)
+nx = 4 # nb de succès voulus
+np = 0.1 # la proba que la communication échoue
+dgeom(nx, np)
+
+# c)
+
+# 0.045, voir iPad
+
+
